ff3f8111d93aa1ff14528d3615d912f037c6270b
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-05-17 10:38:59 +0600

    Add search markup

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Header = styled.h1``;
+const Label = styled.label``;
+const Input = styled.input``;
 
 const Search = () => {
   const [state, setState] = useState({
@@ -7,6 +13,15 @@ const Search = () => {
     loading: false,
     message: '',
   });
+
+  return (
+    <Container>
+      <Header>Live search</Header>
+      <Label htmlFor="search-input">
+        <Input type="text" id="search-input" value="" />
+      </Label>
+    </Container>
+  );
 };
 
 export default Search;
