f2dee9f2caeb85d9aecf815715c7c0ecc49d21f6
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-05-17 14:14:46 +0600

    Move data fetch to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import Search from './Search';
 
 const Container = styled.div``;
 
+type Users = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
 const App = () => {
+  const [users, setUsers] = useState<Users[] | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+        const result = await axios.get(usersUrl);
+        setUsers(result.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchResult();
+  }, []);
   return (
     <Container>
       <Search />
--- src/Search.tsx
+++ src/Search.tsx
@@ -1,57 +1,14 @@
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 
 const Container = styled.div``;
-const Header = styled.h1``;
 const Label = styled.label``;
 const Input = styled.input``;
 
-type Users = {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-};
-
 const Search = () => {
-  const [users, setUsers] = useState<Users[] | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchResult = async () => {
-      try {
-        const result = await axios.get(usersUrl);
-        setUsers(result.data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchResult();
-  }, []);
-
-  console.log(users);
-
-  const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const element = event.currentTarget as HTMLInputElement;
-    const query = element.value;
-    console.log(query);
-  };
-
-  const usersUrl = 'https://jsonplaceholder.typicode.com/users';
-
   return (
     <Container>
-      {loading ? <Header>Loading</Header> : <Header>Live search</Header>}
       <Label htmlFor="search-input">
-        <Input
-          type="text"
-          id="search-input"
-          value=""
-          placeholder="Search"
-          onChange={event => handleInputChange(event)}
-        />
+        <Input type="text" id="search-input" value="" placeholder="Search" />
       </Label>
     </Container>
   );
