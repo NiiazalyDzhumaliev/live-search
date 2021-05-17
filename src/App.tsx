76ab14a3bf5a662e014a7557ff23306a764e48aa
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-05-17 19:01:28 +0600

    Fix appearing on type

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App = () => {
           />
         </Label>
       </InputContainer>
-      <DropDown users={filteredUsers} />
+      {searchName !== '' ? <DropDown users={filteredUsers} /> : null}
     </Container>
   );
 };
