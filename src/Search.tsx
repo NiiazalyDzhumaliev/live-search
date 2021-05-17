a98fef521b6cecdffd8ea98072288c834ba79674
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-05-17 13:52:33 +0600

    Add loading state

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Container = styled.div``;
 const Header = styled.h1``;
 const Label = styled.label``;
 const Input = styled.input``;
 
+type Users = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
 const Search = () => {
-  const [state, setState] = useState({
-    query: '',
-    results: [],
-    loading: false,
-    message: '',
-  });
+  const [users, setUsers] = useState<Users[] | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+        const result = await axios.get(usersUrl);
+        setUsers(result.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchResult();
+  }, []);
+
+  console.log(users);
 
   const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
     const element = event.currentTarget as HTMLInputElement;
     const query = element.value;
-    setState({
-      ...state,
-      query,
-      loading: true,
-      message: '',
-    });
+    console.log(query);
   };
+
+  const usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
   return (
     <Container>
-      <Header>Live search</Header>
+      {loading ? <Header>Loading</Header> : <Header>Live search</Header>}
       <Label htmlFor="search-input">
         <Input
           type="text"
